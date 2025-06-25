712765db145a072bbfc03005f51651bacf264fcf
Thinh Phuc <duonge12@gmail.com>
2025-06-25 14:03:41 +0700

    13

--- api/app.js
+++ api/app.js
@@ -2,23 +2,9 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 
-// Middleware: attach per-request acceptance flag
-app.use((req, res, next) => {
-	const origin = req.headers.origin;
-	if (origin && [""].includes(origin)) {
-		req.isAcceptedOrigin = true;
-		res.setHeader("Access-Control-Allow-Origin", origin);
-		res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-		res.setHeader("Access-Control-Allow-Headers", "*");
-	} else {
-		req.isAcceptedOrigin = false;
-	}
-	next();
-});
-
 // Routes
 app.get("/", (req, res) => {
-	if (req.isAcceptedOrigin) {
+	if (req.headers.origin === "http://127.0.0.1:5500") {
 		return res.send("Request default");
 	} else {
 		return res.send("Forbidden default");
@@ -26,7 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/a", (req, res) => {
-	if (req.isAcceptedOrigin) {
+	if (req.headers.origin === "http://127.0.0.1:5500") {
 		return res.send("Request a");
 	} else {
 		return res.send("Forbidden a");
@@ -34,7 +20,7 @@ app.get("/a", (req, res) => {
 });
 
 app.get("/b", (req, res) => {
-	if (req.isAcceptedOrigin) {
+	if (req.headers.origin === "http://127.0.0.1:5500") {
 		return res.send("Request b");
 	} else {
 		return res.send("Forbidden b");
