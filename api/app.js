5885a15a867ee742f5246d397ee4812d2c9dc9b7
Thinh Phuc <duonge12@gmail.com>
2025-06-25 14:20:45 +0700

    16

--- api/app.js
+++ api/app.js
@@ -4,37 +4,10 @@ const app = express();
 
 app.get("/", (req, res) => {
 	res.setHeader("Access-Control-Allow-Origin", "*"); // optional
+	console.log("Request received at root path");
 	res.send("Request default");
 });
 
-app.get("/a", (req, res) => {
-	if (req.headers.origin === "http://127.0.0.1:5500") {
-		res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-		return res.send("Request a");
-	} else {
-		return res.status(403).send("Forbidden a");
-	}
-});
-
-app.get("/b", (req, res) => {
-	if (req.headers.origin === "http://127.0.0.1:5500") {
-		res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-		return res.send("Request b");
-	} else {
-		return res.status(403).send("Forbidden b");
-	}
-});
-
-// Handle preflight OPTIONS requests
-app.options("*", (req, res) => {
-	const origin = req.headers.origin;
-	if (origin === "http://127.0.0.1:5500") {
-		res.setHeader("Access-Control-Allow-Origin", origin);
-		res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-		res.setHeader("Access-Control-Allow-Headers", "*");
-		return res.sendStatus(204);
-	}
-	res.sendStatus(403);
-});
-
-module.exports = serverless(app);
+module.exports = (req, res) => {
+	app(req, res);
+};
