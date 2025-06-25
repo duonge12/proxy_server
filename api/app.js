b076bf7107a6042f6adcbcd082a978dc488cace1
Thinh Phuc <duonge12@gmail.com>
2025-06-25 12:25:31 +0700

    test

--- api/app.js
+++ api/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
 	res.send("Hello from Express without serverless-http!");
 });
+app.get("/a", (req, res) => {
+	res.send("This is a");
+});
 
 module.exports = (req, res) => {
 	app(req, res); // this wraps your express app manually
